e7425b42939b1e4b5c18b553ae860bf1a9ec68ed
andrew <asnolin@uvm.edu>
2018-04-24 19:59:39 -0400

    worked on project.ml

--- project.ml
+++ project.ml
@@ -1,4 +1,12 @@
-(*Final Project by ANdrew Nolin and Jared Wiggett *)
+(*Final Project by Andrew Nolin and Jared Wiggett *)
+(*Course: UVM CS 225 spring 2018*)
+(*TODO for checkpoint:
+        * small step semantics for 9.1 simply typed lambda calc
+        * type checker for simply typed lambda calc
+*)
+
+type expressions = 
+        |
 
 
 (*types*)
